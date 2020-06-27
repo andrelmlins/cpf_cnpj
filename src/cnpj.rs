8f280b12c9910003e742e5a2a04e0ac7f46d51e8
André Lins <andrelucas01@hotmail.com>
2020-06-27 08:55:31 -0300

    Inserindo gerador de cnpj

--- src/cnpj.rs
+++ src/cnpj.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub fn validate(valor: &str) -> bool {
     let numbers = valor
         .chars()
@@ -9,24 +11,42 @@ pub fn validate(valor: &str) -> bool {
     }
 
     let digit_one = validate_first_digit(&numbers);
-    if digit_one != numbers[12].to_string().parse::<i16>().unwrap() {
+    if digit_one != numbers[12].to_string().parse::<usize>().unwrap() {
         return false;
     }
 
     let digit_second = validate_second_digit(&numbers);
-    if digit_second != numbers[13].to_string().parse::<i16>().unwrap() {
+    if digit_second != numbers[13].to_string().parse::<usize>().unwrap() {
         return false;
     }
 
     return true;
 }
 
-fn validate_first_digit(numbers: &Vec<char>) -> i16 {
-    let array: [i16; 12] = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+pub fn generate() -> String {
+    let mut rng = rand::thread_rng();
+    const CHARSET: &[u8] = b"0123456789";
+
+    let mut vec: Vec<char> = (0..8)
+        .map(|_| {
+            let idx = rng.gen_range(0, CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    vec.extend(vec!['0', '0', '0', '1']);
+    vec.push(CHARSET[validate_first_digit(&vec)] as char);
+    vec.push(CHARSET[validate_second_digit(&vec)] as char);
+
+    return vec.into_iter().collect();
+}
+
+fn validate_first_digit(numbers: &Vec<char>) -> usize {
+    let array: [usize; 12] = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
     let mut _sum = 0;
     for (position, number) in numbers.iter().enumerate() {
         if position <= 11 {
-            let number_int = number.to_string().parse::<i16>().unwrap();
+            let number_int = number.to_string().parse::<usize>().unwrap();
             _sum += number_int * array[position];
         }
     }
@@ -38,12 +58,12 @@ fn validate_first_digit(numbers: &Vec<char>) -> i16 {
     return 11 - result;
 }
 
-fn validate_second_digit(numbers: &Vec<char>) -> i16 {
-    let array: [i16; 13] = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+fn validate_second_digit(numbers: &Vec<char>) -> usize {
+    let array: [usize; 13] = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
     let mut _sum = 0;
     for (position, number) in numbers.iter().enumerate() {
         if position <= 12 {
-            let number_int = number.to_string().parse::<i16>().unwrap();
+            let number_int = number.to_string().parse::<usize>().unwrap();
             _sum += number_int * array[position];
         }
     }
